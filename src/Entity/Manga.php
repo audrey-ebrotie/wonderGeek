08701bcc33a43b875f69243c13bc7e5bd7e1db3c
Dylan <dylan0510@live.fr>
2022-01-07 16:32:42 +0100

    changement pages event

--- src/Entity/Manga.php
+++ src/Entity/Manga.php
@@ -6,6 +6,7 @@ use App\Repository\MangaRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MangaRepository::class)]
 class Manga
